e3ba224a21d037dd298bb18b30e47bcd800a7b39
Yego Collins Kimeli <125864533+yegocollins@users.noreply.github.com>
2025-04-02 00:36:10 +0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL || "https://masomohub-ac-ke-taupe.vercel.app/api";
 
 const api = axios.create({
   baseURL: API_URL,
